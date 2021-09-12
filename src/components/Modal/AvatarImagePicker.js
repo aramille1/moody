b9b9b43420f3f390926a5014d81f2c8f21530839
Ramil <aramille@gmail.com>
2021-09-12 14:10:35 +0200

    cleaned up a little

--- src/components/Modal/AvatarImagePicker.js
+++ src/components/Modal/AvatarImagePicker.js
@@ -1,229 +1,233 @@
-import React, { useContext } from 'react';
-import { ImageBackground,Image, TextInput, StyleSheet, Text, TouchableOpacity,ScrollView, View, Platform, Alert } from 'react-native';
+import React, {useContext} from 'react';
+import {
+  ImageBackground,
+  Image,
+  TextInput,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  View,
+  Platform,
+  Alert,
+} from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Animated from 'react-native-reanimated';
 import BottomSheet from 'reanimated-bottom-sheet';
-import { MoodContext } from '../../../App';
+import {MoodContext} from '../../../App';
 import uploadImg from '../../assets/images/camera-icon.png';
 
-export default function AvatarImagePicker({ setImageProp, imageIsUploaded }) {
+export default function AvatarImagePicker({setImageProp, imageIsUploaded}) {
+  const [image, setImage] = React.useState();
+  const mood = useContext(MoodContext);
+  const [username, setUsername] = React.useState('username');
 
-    const [image, setImage] = React.useState();
-    const mood = useContext(MoodContext)
-    const [username, setUsername] = React.useState('username')
+  // const cleanupImages = () => {
+  //     ImagePicker.clean()
+  //         .then(() => {
+  //             console.log('removed tmp images from tmp directory');
+  //         })
+  //         .catch((e) => {
+  //             alert(e);
+  //         });
+  // }
 
-    // const cleanupImages = () => {
-    //     ImagePicker.clean()
-    //         .then(() => {
-    //             console.log('removed tmp images from tmp directory');
-    //         })
-    //         .catch((e) => {
-    //             alert(e);
-    //         });
-    // }
+  // const deleteImage = () => {
+  //     ImagePicker.cleanSingle(image ? image.uri : null)
+  //         .then(() => {
+  //             console.log(`removed tmp image ${image.uri} from tmp directory`);
+  //         })
+  //         .catch((e) => {
+  //             alert(e);
+  //         });
+  // }
 
-    // const deleteImage = () => {
-    //     ImagePicker.cleanSingle(image ? image.uri : null)
-    //         .then(() => {
-    //             console.log(`removed tmp image ${image.uri} from tmp directory`);
-    //         })
-    //         .catch((e) => {
-    //             alert(e);
-    //         });
-    // }
+  const takePhotoFromCamera = () => {
+    ImagePicker.openCamera({
+      compressImageMaxWidth: 300,
+      compressImageMaxHeight: 300,
+      cropping: true,
+      compressImageQuality: 0.7,
+    })
+      .then((image) => {
+        console.log(image);
+        const imageUrl = Platform.OS === 'ios' ? image.sourceURL : image.path;
+        setImage(imageUrl);
+        setImageProp(imageUrl);
+        sheetRef.current.snapTo(1);
+      })
+      .catch((error) => {
+        alert(error);
+      });
+  };
 
+  const choosePhotoFromLibrary = () => {
+    ImagePicker.openPicker({
+      width: 300,
+      height: 300,
+      cropping: true,
+      compressImageQuality: 0.7,
+    }).then((image) => {
+      const imageUrl = Platform.OS === 'ios' ? image.sourceURL : image.path;
+      setImage(imageUrl);
+      setImageProp(imageUrl);
+      sheetRef.current.snapTo(1);
+    });
+  };
 
+  const renderInner = () => (
+    <View style={styles.panel}>
+      <View style={{alignItems: 'center'}}>
+        <Text style={styles.panelTitle}>Upload Photo</Text>
+        <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
+      </View>
+      <TouchableOpacity
+        style={styles.panelButton}
+        onPress={takePhotoFromCamera}>
+        <Text style={styles.panelButtonTitle}>Take Photo</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.panelButton}
+        onPress={choosePhotoFromLibrary}>
+        <Text style={styles.panelButtonTitle}>Choose From Library</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.panelButton}
+        onPress={() => sheetRef.current.snapTo(1)}>
+        <Text style={styles.panelButtonTitle}>Cancel</Text>
+      </TouchableOpacity>
+    </View>
+  );
 
-    const takePhotoFromCamera = () => {
-        ImagePicker.openCamera({
-            compressImageMaxWidth: 300,
-            compressImageMaxHeight: 300,
-            cropping: true,
-            compressImageQuality: 0.7
-        }).then(image => {
-            console.log(image);
-            const imageUrl = Platform.OS === 'ios' ? image.sourceURL : image.path
-            setImage(imageUrl)
-            setImageProp(imageUrl)
-            sheetRef.current.snapTo(1);
-        }).catch(error => {
-            alert(error)
-        })
-    }
+  const renderHeader = () => (
+    <View style={styles.header}>
+      <View style={styles.panelHeader}>
+        <View style={styles.panelHandle} />
+      </View>
+    </View>
+  );
+  const sheetRef = React.useRef();
+  const fall = new Animated.Value(1);
 
-    const choosePhotoFromLibrary = () => {
-        ImagePicker.openPicker({
-            width: 300,
-            height: 300,
-            cropping: true,
-            compressImageQuality: 0.7
-        }).then(image => {
-            const imageUrl = Platform.OS === 'ios' ? image.sourceURL : image.path
-            setImage(imageUrl)
-            setImageProp(imageUrl)
-            sheetRef.current.snapTo(1);
-        });
-    }
-
-
-    const renderInner = () => (
-        <View style={styles.panel}>
-            <View style={{ alignItems: 'center' }}>
-                <Text style={styles.panelTitle}>Upload Photo</Text>
-                <Text style={styles.panelSubtitle}>Choose Your Profile Picture</Text>
-            </View>
-            <TouchableOpacity style={styles.panelButton} onPress={takePhotoFromCamera}>
-                <Text style={styles.panelButtonTitle}>Take Photo</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.panelButton} onPress={choosePhotoFromLibrary}>
-                <Text style={styles.panelButtonTitle}>Choose From Library</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                style={styles.panelButton}
-                onPress={() => sheetRef.current.snapTo(1)}>
-                <Text style={styles.panelButtonTitle}>Cancel</Text>
-            </TouchableOpacity>
-        </View>
-    );
-
-    const renderHeader = () => (
-        <View style={styles.header}>
-            <View style={styles.panelHeader}>
-                <View style={styles.panelHandle} />
-            </View>
-        </View>
-    );
-    const sheetRef = React.useRef();
-    const fall = new Animated.Value(1);
-
-    return (
-        <>
-            <BottomSheet
-                ref={sheetRef}
-                snapPoints={[420, -200]}
-                renderContent={renderInner}
-                renderHeader={renderHeader}
-                callbackNode={fall}
-                initialSnap={1}
-                enabledGestureInteraction={true}
-            />
-                <Animated.View style={{
-                    // margin: 20,
-                    opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)),
-                }}>
-                        <TouchableOpacity onPress={() => sheetRef.current.snapTo(0)}>
-                        <Image
-                        style={{width:55,height:55}}
-          source={uploadImg}
-        />
-                                {/* <Text style={{borderWidth: 1, borderColor: 'grey', borderRadius: 10,padding: 7,}}>upload photo</Text> */}
-                        </TouchableOpacity>
-                        
-                </Animated.View>
-        </>
-    );
+  return (
+    <>
+      <BottomSheet
+        ref={sheetRef}
+        snapPoints={[420, -200]}
+        renderContent={renderInner}
+        renderHeader={renderHeader}
+        callbackNode={fall}
+        initialSnap={1}
+        enabledGestureInteraction={true}
+      />
+      <Animated.View
+        style={{
+          // margin: 20,
+          opacity: Animated.add(0.1, Animated.multiply(fall, 1.0)),
+        }}>
+        <TouchableOpacity onPress={() => sheetRef.current.snapTo(0)}>
+          <Image style={{width: 55, height: 55}} source={uploadImg} />
+          {/* <Text style={{borderWidth: 1, borderColor: 'grey', borderRadius: 10,padding: 7,}}>upload photo</Text> */}
+        </TouchableOpacity>
+      </Animated.View>
+    </>
+  );
 }
 
-
-
 const styles = StyleSheet.create({
-    // container: {
-    //     flex: 1,
+  // container: {
+  //     flex: 1,
 
-    // },
-    commandButton: {
-        padding: 15,
-        borderRadius: 10,
-        backgroundColor: '#FF6347',
-        alignItems: 'center',
-        marginTop: 10,
-    },
-    panel: {
-        padding: 20,
-        backgroundColor: '#FFFFFF',
-        paddingTop: 20,
-        borderRightWidth: 1,
-        borderLeftWidth: 1,
-        borderRightColor: 'black',
-        borderLeftColor: 'black'
+  // },
+  commandButton: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: '#FF6347',
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  panel: {
+    padding: 20,
+    backgroundColor: '#FFFFFF',
+    paddingTop: 20,
+    borderRightWidth: 1,
+    borderLeftWidth: 1,
+    borderRightColor: 'black',
+    borderLeftColor: 'black',
 
-        // borderTopLeftRadius: 20,
-        // borderTopRightRadius: 20,
-        // shadowColor: '#000000',
-        // shadowOffset: {width: 0, height: 0},
-        // shadowRadius: 5,
-        // shadowOpacity: 0.4,
-    },
-    header: {
-        backgroundColor: '#FFFFFF',
-        shadowColor: '#333333',
-        shadowOffset: { width: -1, height: -3 },
-        shadowRadius: 2,
-        shadowOpacity: 0.4,
-        // elevation: 5,
-        paddingTop: 20,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        borderColor: 'black',
-        borderWidth: 1,
-        borderBottomWidth: 0
-    },
-    panelHeader: {
-        alignItems: 'center',
-    },
-    panelHandle: {
-        width: 40,
-        height: 8,
-        borderRadius: 4,
-        backgroundColor: '#00000040',
-        marginBottom: 10,
-    },
-    panelTitle: {
-        fontSize: 27,
-        height: 35,
-    },
-    panelSubtitle: {
-        fontSize: 14,
-        color: 'gray',
-        height: 30,
-        marginBottom: 10,
-    },
-    panelButton: {
-        padding: 13,
-        borderRadius: 10,
-        backgroundColor: '#009387',
-        alignItems: 'center',
-        marginVertical: 7,
-    },
-    panelButtonTitle: {
-        fontSize: 17,
-        fontWeight: 'bold',
-        color: 'white',
-    },
-    action: {
-        flexDirection: 'row',
-        marginTop: 10,
-        marginBottom: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#f2f2f2',
-        paddingBottom: 5,
-    },
-    actionError: {
-        flexDirection: 'row',
-        marginTop: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#FF0000',
-        paddingBottom: 5,
-    },
-    textInput: {
-        flex: 1,
-        marginTop: Platform.OS === 'ios' ? 0 : -12,
-        paddingLeft: 10,
-        color: '#05375a',
-    },
-
-     
-
-
+    // borderTopLeftRadius: 20,
+    // borderTopRightRadius: 20,
+    // shadowColor: '#000000',
+    // shadowOffset: {width: 0, height: 0},
+    // shadowRadius: 5,
+    // shadowOpacity: 0.4,
+  },
+  header: {
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#333333',
+    shadowOffset: {width: -1, height: -3},
+    shadowRadius: 2,
+    shadowOpacity: 0.4,
+    // elevation: 5,
+    paddingTop: 20,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderBottomWidth: 0,
+  },
+  panelHeader: {
+    alignItems: 'center',
+  },
+  panelHandle: {
+    width: 40,
+    height: 8,
+    borderRadius: 4,
+    backgroundColor: '#00000040',
+    marginBottom: 10,
+  },
+  panelTitle: {
+    fontSize: 27,
+    height: 35,
+  },
+  panelSubtitle: {
+    fontSize: 14,
+    color: 'gray',
+    height: 30,
+    marginBottom: 10,
+  },
+  panelButton: {
+    padding: 13,
+    borderRadius: 10,
+    backgroundColor: '#009387',
+    alignItems: 'center',
+    marginVertical: 7,
+  },
+  panelButtonTitle: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#f2f2f2',
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
 });
--- src/screens/Main.js
+++ src/screens/Main.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, {Component, useEffect} from 'react';
 import {
   PermissionsAndroid,
   Platform,
@@ -13,45 +13,42 @@ import {
   StatusBar,
   ScrollView,
   TouchableOpacity,
-  ActivityIndicator
-} from "react-native";
-import Contacts from "react-native-contacts";
-import ListItem from "../components/ListItem/index"
+  ActivityIndicator,
+} from 'react-native';
+import Contacts from 'react-native-contacts';
+import ListItem from '../components/ListItem/index';
 import * as Animatable from 'react-native-animatable';
-import Avatar from "../components/Avatar/index"
+import Avatar from '../components/Avatar/index';
 import SearchBar from '../components/SearchBar/index';
 import Icon from 'react-native-vector-icons/Ionicons';
 import Modal from 'react-native-modal';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import { MoodContext } from '../../App';
-import ProfileImagePicker from '../components/Modal/ProfileImagePicker'
-import firestore from '@react-native-firebase/firestore'
-import { firebase } from "@react-native-firebase/auth";
+import {MoodContext} from '../../App';
+import ProfileImagePicker from '../components/Modal/ProfileImagePicker';
+import firestore from '@react-native-firebase/firestore';
+import {firebase} from '@react-native-firebase/auth';
 import auth from '@react-native-firebase/auth';
 
-export default function Main({ navigation }) {
-  mood = React.useContext(MoodContext)
+export default function Main({navigation}) {
+  mood = React.useContext(MoodContext);
 
+  const [profileImg, setProfileImg] = React.useState();
+  const [contacts, setContacts] = React.useState([]);
+  const [searchPlaceholder, setSearchPlaceholder] = React.useState('Search');
+  const [typeText, setTypeText] = React.useState(null);
+  const [loading, setLoading] = React.useState(true);
+  const [visible, setVisible] = React.useState(false);
+  const [username, setUsername] = React.useState('username');
 
-
-  const [profileImg, setProfileImg] = React.useState()
-  const [contacts, setContacts] = React.useState([])
-  const [searchPlaceholder, setSearchPlaceholder] = React.useState('Search')
-  const [typeText, setTypeText] = React.useState(null)
-  const [loading, setLoading] = React.useState(true)
-  const [visible, setVisible] = React.useState(false)
-  const [username, setUsername] = React.useState('username')
-
-  const [users, setUsers] = React.useState([])
-  const [usersToRender, setUsersToRender] = React.useState([])
+  const [users, setUsers] = React.useState([]);
+  const [usersToRender, setUsersToRender] = React.useState([]);
 
   // if you want to read/write the contact note field on iOS, this method has to be called
   // WARNING: by enabling notes on iOS, a valid entitlement file containing the note entitlement as well as a separate
   //          permission has to be granted in order to release your app to the AppStore. Please check the README.md
   //          for further information.
   // Contacts.iosEnableNotesUsage(false);
-
 
   //  async componentDidMount() {
   //    if (Platform.OS === "android") {
@@ -67,123 +64,95 @@ export default function Main({ navigation }) {
   //  }
 
   useEffect(() => {
-
-    // getUsers()
-    // alert(1)
-
-    if (Platform.OS === "android") {
+    if (Platform.OS === 'android') {
       PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.READ_CONTACTS, {
-        title: "Contacts",
-        message: "This app would like to view your contacts."
+        title: 'Contacts',
+        message: 'This app would like to view your contacts.',
       }).then(() => {
-          getUsers2();
-        
+        getUsers();
       });
     } else {
-      alert(2/3)
-
       loadContacts();
     }
-  }, [])
+  }, []);
 
-  // const getUser = () => {
-  //   // const userDocument = await firestore().collection("users").doc('TpZwxmTlnmbzQSaeRQk3').get()
-  //   // console.log(userDocument)
-  //   let tempUsers = []
-  //   firestore()
-  //   .collection('users')
-  //   .onSnapshot(docs => {
-  //     docs.forEach(user => {
-  //         tempUsers.push(user.data())
-  //       })
-  //     })
-  //     setUsers(tempUsers)
-  // }
   const getUsers = () => {
-
-}
-
-const getUsers2 = () => {
-  let numbers = []
-  Contacts.getAll()
-  .then(contacts => {
-    console.log(contacts)
-    contacts.forEach(contact =>{
-      contact.phoneNumbers.forEach(num =>{
-        let trimmedNum = num.number.replace(/\s/g, "");
-        numbers.push(trimmedNum)
+    let numbers = [];
+    Contacts.getAll()
+      .then((contacts) => {
+        console.log(contacts);
+        contacts.forEach((contact) => {
+          contact.phoneNumbers.forEach((num) => {
+            let trimmedNum = num.number.replace(/\s/g, '');
+            numbers.push(trimmedNum);
+          });
+        });
+        setContacts(numbers);
+        let tempUsers = [];
+        firestore()
+          .collection('users')
+          .onSnapshot((docs) => {
+            docs.forEach((user) => {
+              if (
+                numbers.includes(user.data().user.phoneNumber) &&
+                user.data().user.phoneNumber != auth()._user._user.phoneNumber
+              ) {
+                tempUsers.push(user.data());
+                console.log('includes!');
+              }
+            });
+            setUsers(tempUsers);
+            setLoading(false);
+          });
       })
-    })
-    setContacts(numbers)
-    let tempUsers = []
-    firestore()
-    .collection('users')
-    .onSnapshot(docs => {
-      
-      docs.forEach(user => {
-        if(numbers.includes(user.data().user.phoneNumber) && user.data().user.phoneNumber != auth()._user._user.phoneNumber){
-          tempUsers.push(user.data())
-          console.log('includes!')
-        }
-    })
-      setUsers(tempUsers)
-      setLoading(false)
-    })
-  })
-  .catch(e => {
-    setLoading(false)
-  });
+      .catch((e) => {
+        setLoading(false);
+      });
 
-Contacts.getCount().then(count => {
-  setSearchPlaceholder(`Search ${count} contacts`)
-});
-Contacts.checkPermission();
-}
-
-
-  // here we compare numbers in phone contant list and in database list
-
-
+    Contacts.getCount().then((count) => {
+      setSearchPlaceholder(`Search ${count} contacts`);
+    });
+    Contacts.checkPermission();
+  };
 
   const search = (text) => {
     const phoneNumberRegex = /\b[\+]?[(]?[0-9]{2,6}[)]?[-\s\.]?[-\s\/\.0-9]{3,15}\b/m;
     const emailAddressRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-    if (text === "" || text === null) {
+    if (text === '' || text === null) {
       this.loadContacts();
     } else if (phoneNumberRegex.test(text)) {
-      Contacts.getContactsByPhoneNumber(text).then(contacts => {
+      Contacts.getContactsByPhoneNumber(text).then((contacts) => {
         // this.setState({ contacts });
-        setContacts(contacts)
+        setContacts(contacts);
       });
     } else if (emailAddressRegex.test(text)) {
-      Contacts.getContactsByEmailAddress(text).then(contacts => {
+      Contacts.getContactsByEmailAddress(text).then((contacts) => {
         // this.setState({ contacts });
-        setContacts(contacts)
+        setContacts(contacts);
       });
     } else {
-      Contacts.getContactsMatchingString(text).then(contacts => {
+      Contacts.getContactsMatchingString(text).then((contacts) => {
         // this.setState({ contacts });
-        setContacts(contacts)
+        setContacts(contacts);
       });
     }
-  }
+  };
 
   const onPressContact = (contact) => {
     var text = typeText;
     // this.setState({ typeText: null });
-    setTypeText(null)
-    if (text === null || text === '')
-      Contacts.openExistingContact(contact)
+    setTypeText(null);
+    if (text === null || text === '') Contacts.openExistingContact(contact);
     else {
       var newPerson = {
         recordID: contact.recordID,
-        phoneNumbers: [{ label: 'mobile', number: text }]
-      }
-      Contacts.editExistingContact(newPerson).then(contact => {
+        phoneNumbers: [{label: 'mobile', number: text}],
+      };
+      Contacts.editExistingContact(newPerson).then((contact) => {
         //contact updated
       });
     }
-  }
+  };
 
   // const addNew = () => {
   //   Contacts.openContactForm({}).then(contact => {
@@ -198,24 +167,22 @@ Contacts.checkPermission();
   // }
 
   const onSignIn = () => {
-    mood.setUsername(username)
+    mood.setUsername(username);
     // mood.setImg(profileImg)
-    console.log(mood.moodObj, "moodObj")
-    setVisible(false)
-
-  }
+    console.log(mood.moodObj, 'moodObj');
+    setVisible(false);
+  };
 
   const onUserPress = (user) => {
-    console.log(user)
+    console.log(user);
+  };
 
-  }
-
-  const getAvatarInitials = textString => {
-    if (!textString) return "";
+  const getAvatarInitials = (textString) => {
+    if (!textString) return '';
 
     const text = textString.trim();
 
-    const textSplit = text.split(" ");
+    const textSplit = text.split(' ');
 
     if (textSplit.length <= 1) return text.charAt(0);
 
@@ -227,23 +194,20 @@ Contacts.checkPermission();
 
   const test = () => {
     // getUsers()
-  }
+  };
 
   return (
     <>
       <SafeAreaView style={styles.container}>
-        <StatusBar backgroundColor='#fff' barStyle="dark-content" />
+        <StatusBar backgroundColor="#fff" barStyle="dark-content" />
         <View
           style={{
             paddingLeft: 100,
             paddingRight: 100,
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-        </View>
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}></View>
         {/* <Button title="Add new" onPress={() => addNew()} /> */}
-
 
         {/* <SearchBar
           searchPlaceholder={searchPlaceholder}
@@ -261,43 +225,48 @@ Contacts.checkPermission();
         </View> */}
 
         {/* this is my users from FireBase */}
-        {
-          loading === true ?
-            (
-              <View style={styles.spinner}>
-                <ActivityIndicator size="large" color="#0000ff" />
-              </View>
-            ) :
-            (
-              <ScrollView >
-              {users.map((user,index) => {
-                return (
-                  <ListItem
-                    leftElement={
-                     <Image
-                       source={{ uri: user.image }}
-                       style={styles.userProfileImage}>
-                     </Image>
-                    }
-                    key={index}
-                    title={user.username}
-                   onPress={() => navigation.navigate('OtherMood', { screen: 'OtherMood', params: { user } })}
-                  />
-                );
-              })}
-            </ScrollView>
-            )
-        }
-        {/* this is the end my users from FireBase */}
+        {loading === true ? (
+          <View style={styles.spinner}>
+            <ActivityIndicator size="large" color="#0000ff" />
+          </View>
+        ) : (
+          <ScrollView>
+            {users.map((user, index) => {
+              return (
+                <ListItem
+                  leftElement={
+                    <Image
+                      source={{uri: user.image}}
+                      style={styles.userProfileImage}></Image>
+                  }
+                  key={index}
+                  title={user.username}
+                  onPress={() =>
+                    navigation.navigate('OtherMood', {
+                      screen: 'OtherMood',
+                      params: {user},
+                    })
+                  }
+                />
+              );
+            })}
+          </ScrollView>
+        )}
 
-        <View style={{
-          alignItems: 'flex-end',
-          justifyContent: 'flex-end',
-          flex: 1,
-          marginBottom: 50,
-          marginRight: 30
-        }}>
-          <Icon onPress={() => navigation.navigate('EditMood')} name="create-outline" size={50} color="#373737" />
+        <View
+          style={{
+            alignItems: 'flex-end',
+            justifyContent: 'flex-end',
+            flex: 1,
+            marginBottom: 50,
+            marginRight: 30,
+          }}>
+          <Icon
+            onPress={() => navigation.navigate('EditMood')}
+            name="create-outline"
+            size={50}
+            color="#373737"
+          />
           {/* <Button title="check" onPress={test} /> */}
         </View>
       </SafeAreaView>
@@ -307,24 +276,24 @@ Contacts.checkPermission();
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
   },
   spinner: {
     flex: 1,
     flexDirection: 'column',
-    alignContent: "center",
-    justifyContent: "center"
+    alignContent: 'center',
+    justifyContent: 'center',
   },
   inputStyle: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
-    textAlign: "center"
+    textAlign: 'center',
   },
   container3: {
     flex: 1,
     backgroundColor: '#009387',
-    justifyContent: 'center'
+    justifyContent: 'center',
   },
   footer: {
     // flex: Platform.OS === 'ios' ? 3 : 9,
@@ -336,14 +305,14 @@ const styles = StyleSheet.create({
   },
   text_footer: {
     color: '#05375a',
-    fontSize: 18
+    fontSize: 18,
   },
   action: {
     flexDirection: 'row',
     marginTop: 10,
     borderBottomWidth: 1,
     borderBottomColor: '#f2f2f2',
-    paddingBottom: 5
+    paddingBottom: 5,
   },
   textInput: {
     flex: 1,
@@ -359,7 +328,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     flexDirection: 'row',
     borderRadius: 10,
-    margin: 10
+    margin: 10,
   },
   btnSkip: {
     backgroundColor: '#fff',
@@ -373,9 +342,13 @@ const styles = StyleSheet.create({
   textSign: {
     color: '#fff',
     fontWeight: 'bold',
-
   },
-  userTouchable: { flexDirection: 'row', alignItems: 'center', marginLeft: 20, marginTop: 20},
-  userProfileImage: { borderRadius: 45, height: 50, width: 50 },
-  userUsername: { marginLeft: 20, fontSize: 15 },
+  userTouchable: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginLeft: 20,
+    marginTop: 20,
+  },
+  userProfileImage: {borderRadius: 45, height: 50, width: 50},
+  userUsername: {marginLeft: 20, fontSize: 15},
 });
