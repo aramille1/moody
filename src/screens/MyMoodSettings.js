55396e025b6cdb013783929b64cedbfa787eaffd
Ramil <aramille@gmail.com>
2021-09-18 16:22:10 +0200

    cleaned up

--- src/screens/MyMoodSettings.js
+++ src/screens/MyMoodSettings.js
@@ -10,29 +10,22 @@ import {
   TextInput,
   ScrollView,
   ActivityIndicator,
-  Alert,
 } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 import auth, {firebase} from '@react-native-firebase/auth';
 import MultiSlider from '@ptomasroos/react-native-multi-slider';
-import Emoji from 'react-native-emoji';
 import FlashMessage, {showMessage} from 'react-native-flash-message';
-// import CustomMarker from './CustomMarker/CustomMarker.js';
-import SetAvatarMessageModal from '../components/Modal/SetAvatarMessageModal';
 import LeftSideMessageModal from '../components/Modal/LeftSideMessageModal';
 import RightSideMessageModal from '../components/Modal/RightSideMessageModal';
+import SetAvatarMessageModal from '../components/Modal/SetAvatarMessageModal';
 import {MoodContext} from '../../App';
 import AvatarImagePicker from '../components/Modal/AvatarImagePicker';
 import gradient from '../assets/images/gradient.png';
-import bg from '../assets/images/css-gradient.png';
-// import Icon from 'react-native-vector-icons/Ionicons';
 import firestore from '@react-native-firebase/firestore';
-import userImg from '../assets/images/user.png';
 import Modal from 'react-native-modal';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import storage from '@react-native-firebase/storage';
-
 import CustomLabel from '../components/CustomLabel';
 
 export default function MyMoodSettings() {
@@ -43,8 +36,6 @@ export default function MyMoodSettings() {
   const [rightSideMessage, setrightSideMessage] = React.useState('happy');
   const [user, setUser] = React.useState([]);
   const [userId, setUserId] = React.useState();
-  const [phoneNum, setPhoneNum] = React.useState();
-  const [phoneUid, setPhoneUid] = React.useState();
   const mood = useContext(MoodContext);
   const [username, setUsername] = React.useState('username');
   const [visible, setVisible] = React.useState(false);
@@ -52,7 +43,7 @@ export default function MyMoodSettings() {
   const [imageIsUploaded, setImageIsUploaded] = React.useState(false);
   const [transferred, setTransferred] = React.useState(0);
   const [isLoading, setIsLoading] = React.useState(true);
-  
+
   useEffect(() => {
     firestore()
       .collection('users')
@@ -65,16 +56,19 @@ export default function MyMoodSettings() {
             setUser(users);
             users.forEach((user) => {
               setTempSliderValues(user.sliderValues),
-                setUsername(user.username),
-                setImage(user.image),
-                setMessage(user.message)
-                setleftSideMessage(user.leftSideMessage),
-                setrightSideMessage(user.rightSideMessage);
+              setUsername(user.username),
+              setImage(user.image),
+              setMessage(user.message);
+              setleftSideMessage(user.leftSideMessage),
+              setrightSideMessage(user.rightSideMessage);
               setIsLoading(false);
             });
           }
         });
       });
+      return () =>{
+        setUser([])
+      }
   }, []);
 
   const submit = () => {
@@ -90,10 +84,9 @@ export default function MyMoodSettings() {
         rightSideMessage: rightSideMessage,
         username: username,
       };
-      console.log(userObject);
       firestore().collection('users').doc(userId).update(userObject);
       showMessage({
-        message: 'Saved!',
+        message: 'Changes Saved!',
         type: 'success',
       });
     }
@@ -162,13 +155,8 @@ export default function MyMoodSettings() {
     return (
       <Image
         source={{uri: image}}
-        style={{
-          borderRadius: 45,
-          height: 90,
-          width: 90,
-          borderWidth: 1,
-          borderColor: '#05375a',
-        }}></Image>
+        style={styles.customMarkerStyles}>
+      </Image>
     );
   };
 
@@ -179,29 +167,13 @@ export default function MyMoodSettings() {
 
   return (
     <>
-      <View
-        // source={bg}
-        style={{
-          backgroundColor: '#fff',
-          flexDirection: 'column',
-          flex: 1,
-          // resizeMode: 'cover',
-          justifyContent: 'space-evenly',
-          alignItems: 'center',
-          // height: '100%',
-        }}>
+      <View style={styles.containerStyle}>
         <StatusBar backgroundColor="#fff" barStyle="dark-content" />
-
         <Modal
           backdropTransitionOutTiming={0}
           onBackdropPress={() => setVisible(false)}
           isVisible={visible}
-          style={{
-            backgroundColor: 'white',
-            borderRadius: 20,
-            position: 'absolute',
-            top: '25%',
-          }}>
+          style={styles.modalStyles}>
           <View style={{marginLeft: 20, marginTop: 30, marginBottom: 0}}>
             <Text style={{color: '#05375a', fontSize: 30}}>
               What's your name?
@@ -221,13 +193,12 @@ export default function MyMoodSettings() {
             </ScrollView>
           </View>
 
-          <View style={styles.buttonSave}>
             <TouchableOpacity style={styles.btnSave} onPress={onSignIn}>
               <Text style={styles.textSign}>Save</Text>
 
               <MaterialIcons name="navigate-next" color="#fff" size={20} />
             </TouchableOpacity>
-          </View>
+
         </Modal>
 
         {isLoading ? (
@@ -236,37 +207,25 @@ export default function MyMoodSettings() {
           </View>
         ) : (
           <>
-             {/* <TouchableOpacity onPress={logout}>
-          <Text
-            style={{
-              borderWidth: 1,
-              borderColor: 'grey',
-              borderRadius: 10,
-              padding: 7,
-            }}>
-            Logout
-          </Text>
-        </TouchableOpacity> */}
+            {/* <TouchableOpacity onPress={logout}>
+                  <Text
+                    style={{
+                      borderWidth: 1,
+                      borderColor: 'grey',
+                      borderRadius: 10,
+                      padding: 7,
+                    }}>
+                    Logout
+                  </Text>
+                </TouchableOpacity> */}
 
             <AvatarImagePicker setImageProp={(image) => setImageFunc(image)} />
-            {/* </View> */}
 
             {/* username */}
             <TouchableOpacity onPress={() => setVisible(true)}>
-              <Text
-                style={{
-                  // marginTop: 10,
-                  fontSize: 38,
-                  fontWeight: 'bold',
-                  color: '#4f6367',
-                  marginBottom: 50,
-                }}>
-                {username}
-              </Text>
+              <Text style={styles.username}>{username}</Text>
             </TouchableOpacity>
-            {/* username */}
 
-            <View style={styles.container}>
               <Animatable.View animation="fadeIn">
                 <ImageBackground source={gradient} style={styles.trackBgImage}>
                   <MultiSlider
@@ -302,93 +261,68 @@ export default function MyMoodSettings() {
                     sliderLength={300}
                     // pressedMarkerStyle={{backgroundColor:'#D3D3D3'}}
                     markerStyle={{height: 50, width: 50}}
-                    onValuesChangeFinish={(values) =>
-                      setTempSliderValues(values)
-                    }
+                    onValuesChangeFinish={(values) => setTempSliderValues(values)}
                   />
                 </ImageBackground>
 
                 {/* sad and happy indicators */}
                 <View style={styles.leftAndRightContainer}>
-                  <View
-                    style={{
-                      flex: 1,
-                      width: 100,
-                      height: '100%',
-                      alignItems: 'flex-start',
-                      marginLeft: 0,
-                      marginTop: 10,
-                    }}>
+                  <View style={styles.leftsideMsg}>
                     <LeftSideMessageModal
                       leftSideMessage={leftSideMessage}
-                      setLeftsideMessageProp={(value) =>
-                        setleftSideMessage(value)
-                      }
-                    />
+                      setLeftsideMessageProp={(value) =>  setleftSideMessage(value)}/>
                   </View>
-                  {/* <View><Text onPress={justChecking}>something</Text></View> */}
-                  <View
-                    style={{
-                      flex: 1,
-                      width: 100,
-                      height: '100%',
-                      alignItems: 'flex-end',
-                      marginTop: 10,
-                      marginRight: 0,
-                    }}>
+
+                  <View style={styles.rightsideMsg}>
                     <RightSideMessageModal
                       rightSideMessage={rightSideMessage}
-                      setRightsideMessageProp={(value) =>
-                        setrightSideMessage(value)
-                      }
-                    />
+                      setRightsideMessageProp={(value) => setrightSideMessage(value)}/>
                   </View>
+                  
                 </View>
                 {/* end of sad and happy indicators */}
               </Animatable.View>
-            </View>
+            {/* end of mood slider */}
 
-        {/* end of mood slider */}
-
-        {uploading ? (
-          <View>
-            <Text>{transferred}% complted</Text>
-            <ActivityIndicator size="large" color="#0000ff" />
-          </View>
-        ) : (
-          <>
-            <FlashMessage position="top" floating />
-
-            <Animatable.View animation="fadeIn">
-              <View style={{borderRadius: 10, backgroundColor: '#009387'}}>
-                <TouchableOpacity
-                  style={{paddingHorizontal: 140, paddingVertical: 15}}
-                  onPress={submit}>
-                  <Text style={{fontSize: 18, color: '#fff'}}>Save</Text>
-                </TouchableOpacity>
+            {uploading ? (
+              <View>
+                <Text>{transferred}% complted</Text>
+                <ActivityIndicator size="large" color="#0000ff" />
               </View>
-            </Animatable.View>
-          </>
-        )}
+            ) : (
+              <>
+                <FlashMessage position="top" floating />
+                <Animatable.View animation="fadeIn">
+                  <View style={{borderRadius: 10, backgroundColor: '#009387'}}>
+                    <TouchableOpacity
+                      style={{paddingHorizontal: 140, paddingVertical: 15}}
+                      onPress={submit}>
+                      <Text style={{fontSize: 18, color: '#fff'}}>Save</Text>
+                    </TouchableOpacity>
+                  </View>
+                </Animatable.View>
+              </>
+            )}
 
-        <View
-          style={{
-            position: 'absolute',
-            top: 175,
-            left:
-              tempSliderValues == 10
-                ? '80%'
-                : tempSliderValues == 0
-                ? '10%'
-                : tempSliderValues == 9
-                ? '80%'
-                : tempSliderValues * 10 + '%',
-          }}>
-          <SetAvatarMessageModal
-            setMessage={(message) => setMessage(message)}
-          />
-        </View>
-        </>
+
+            <View
+              style={{
+                position: 'absolute',
+                top: 175,
+                left:
+                  tempSliderValues == 10
+                    ? '80%'
+                    : tempSliderValues == 0
+                    ? '10%'
+                    : tempSliderValues == 9
+                    ? '80%'
+                    : tempSliderValues * 10 + '%',
+              }}>
+              <SetAvatarMessageModal
+                setMessage={(message) => setMessage(message)}
+              />
+            </View>
+          </>
         )}
       </View>
     </>
@@ -396,7 +330,19 @@ export default function MyMoodSettings() {
 }
 
 const styles = StyleSheet.create({
-
+  containerStyle: {
+    backgroundColor: '#fff',
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  modalStyles: {
+    backgroundColor: 'white',
+    borderRadius: 20,
+    position: 'absolute',
+    top: '25%',
+  },
   leftAndRightContainer: {
     flexDirection: 'row',
     height: 80,
@@ -470,4 +416,33 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     borderRadius: 10,
   },
+  username:{
+    fontSize: 38,
+    fontWeight: 'bold',
+    color: '#4f6367',
+    marginBottom: 50,
+  },
+  leftsideMsg:{
+    flex: 1,
+    width: 100,
+    height: '100%',
+    alignItems: 'flex-start',
+    marginLeft: 0,
+    marginTop: 10,
+  },
+  rightsideMsg:{
+    flex: 1,
+    width: 100,
+    height: '100%',
+    alignItems: 'flex-end',
+    marginTop: 10,
+    marginRight: 0,
+  },
+  customMarkerStyles:{
+    borderRadius: 45,
+    height: 90,
+    width: 90,
+    borderWidth: 1,
+    borderColor: '#05375a',
+  }
 });
